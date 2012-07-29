2ea6aa09ec0c8d3a3508da5b188e20d03c58c9e3
Corey Ehmke <corey@idolhands.com>
2012-07-28 19:31:15 -0500

    Requires; server changes.

--- lib/synack/server.rb
+++ lib/synack/server.rb
@@ -1,34 +1,41 @@
+require 'drb'
+require 'socket'
+
 module Synack
 
   class Server
+
+    DEFAULT_OPTIONS = {
+      host: 'localhost',
+      port: 11113
+    }
 
     attr_reader :host, :port, :socket
 
     # Class methods ================================================================================
 
-    def self.start(args={})
-      @@server = Synack::Server.new(args)
-      DRb.start_service("druby://#{@@server.host}:#{@@server.port}", @@server)
-      DRb.thread.join
+    def self.start(options={})
+      options = DEFAULT_OPTIONS.merge(options)
+      @host = options[:host]
+      @port = options[:port].to_i
+      @@server = Synack::Server.new
+      puts "READY"
+      ::DRb.start_service("druby://#{@host}:#{@port}", @@server)
+      ::DRb.thread.join
     end
 
     def self.stop
-      DRb.stop_service
+      ::DRb.stop_service
     end
 
     # Instance methods =============================================================================
-
-    def initialize(args={})
-      @host   = args[:host] || 'localhost'
-      @port   = args[:port] || 131313
-      @socket = TCPSocket.open(host, port)
-    end
 
     def sanitize(message)
       message.gsub(/[^0-9A-z\.\-\']/, '_')
     end
 
     def send(message)
+      puts message
       system "/Applications/terminal-notifier.app/Contents/MacOS/terminal-notifier --message \"#{sanitize(message)}\""
     end
 
